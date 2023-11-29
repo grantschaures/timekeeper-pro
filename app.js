bb69e2ee84a052f20441ecf4ceac5643e35b3f64
grantschaures <grantschaures32@gmail.com>
2023-11-28 20:13:55 -0800

    minor change to login btn font

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
         res.redirect(`https://${req.header('host')}${req.url}`);
     else
-        next();
+      next();
 });
 
 // Serve static files from the public dir
