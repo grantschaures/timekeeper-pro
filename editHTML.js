090ef917ef65ecf29b55ed16226d3be51cde01f1
grantschaures <grantschaures32@gmail.com>
2024-04-03 12:25:28 -0700

    updated editHTML main js paths

--- editHTML.js
+++ editHTML.js
@@ -8,9 +8,9 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     }
 
     // Sequentially replace each minified JS file reference with the minified version
-    let result = data.replace(/\/js\/index\.js/g, '/js/minified/index.min.js');
-    result = result.replace(/\/js\/navigation\.js/g, '/js/minified/navigation.min.js');
-    result = result.replace(/\/js\/notes\.js/g, '/js/minified/notes.min.js');
+    let result = data.replace(/\/js\/main\/index\.js/g, '/js/minified/index.min.js');
+    result = result.replace(/\/js\/main\/navigation\.js/g, '/js/minified/navigation.min.js');
+    result = result.replace(/\/js\/main\/notes\.js/g, '/js/minified/notes.min.js');
 
     // Write the final result back to the file
     fs.writeFile(filePath, result, 'utf8', (err) => {
