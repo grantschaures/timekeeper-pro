fcfbfbb21b7b5ef5a7bbf5827bcc80d9343bd861
grantschaures <grantschaures32@gmail.com>
2024-04-30 10:43:34 -0700

    final changes for navigation edits

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,58 +1,60 @@
+// scrolling behavior should be tested for manually
+
 describe('Navigation', () => {
     beforeEach(() => {
       cy.visit('http://localhost:3000')
       cy.get('#sub-main-container').invoke('css', 'opacity', '1');
     })
 
-    // it('Home --> Blog Icon Click --> Home Icon Click', () => {
-    //     cy.get('[data-testid="blog-icon"]').click();
+    it('Home --> Blog Icon Click --> Home Icon Click', () => {
+        cy.get('[data-testid="blog-icon"]').click();
 
-    //     cy.get('[data-testid="main"]').should('not.be.visible');
-    //     cy.get('[data-testid="blogContainer"]').should('be.visible');
+        cy.get('[data-testid="main"]').should('not.be.visible');
+        cy.get('[data-testid="blogContainer"]').should('be.visible');
 
-    //     cy.get('[data-testid="home-icon"]').click();
+        cy.get('[data-testid="home-icon"]').click();
 
-    //     cy.get('[data-testid="main"]').should('be.visible');
-    //     cy.get('[data-testid="blogContainer"]').should('not.be.visible');
-    // })
+        cy.get('[data-testid="main"]').should('be.visible');
+        cy.get('[data-testid="blogContainer"]').should('not.be.visible');
+    })
 
-    // it('Home --> Blog Icon Click --> Blog Container Exit Icon Click', () => {
-    //     cy.get('[data-testid="blog-icon"]').click();
-    //     cy.get('[data-testid="blogExit"]').click();
+    it('Home --> Blog Icon Click --> Blog Container Exit Icon Click', () => {
+        cy.get('[data-testid="blog-icon"]').click();
+        cy.get('[data-testid="blogExit"]').click();
 
-    //     cy.get('[data-testid="main"]').should('be.visible');
-    //     cy.get('[data-testid="blogContainer"]').should('not.be.visible');
-    // })
+        cy.get('[data-testid="main"]').should('be.visible');
+        cy.get('[data-testid="blogContainer"]').should('not.be.visible');
+    })
 
-    // it('Home --> Menu Btn --> Blog Btn', () => {
-    //     cy.get('[data-testid="menuBtn"]').click();
-    //     cy.get('[data-testid="blogMenuContainer"]').click();
+    it('Home --> Menu Btn --> Blog Btn', () => {
+        cy.get('[data-testid="menuBtn"]').click();
+        cy.get('[data-testid="blogMenuContainer"]').click();
         
-    //     cy.get('[data-testid="main"]').should('not.be.visible');
-    //     cy.get('[data-testid="blogContainer"]').should('be.visible');
-    // })
+        cy.get('[data-testid="main"]').should('not.be.visible');
+        cy.get('[data-testid="blogContainer"]').should('be.visible');
+    })
 
-    // it('Home --> Menu Btn --> About Btn', () => {
-    //     cy.get('[data-testid="menuBtn"]').click();
-    //     cy.get('[data-testid="aboutMenuContainer"]').click();
+    it('Home --> Menu Btn --> About Btn', () => {
+        cy.get('[data-testid="menuBtn"]').click();
+        cy.get('[data-testid="aboutMenuContainer"]').click();
 
-    //     cy.get('[data-testid="main"]').should('not.be.visible');
-    //     cy.get('[data-testid="aboutContainer"]').should('be.visible');
+        cy.get('[data-testid="main"]').should('not.be.visible');
+        cy.get('[data-testid="aboutContainer"]').should('be.visible');
         
-    //     cy.get('[data-testid="aboutExit"]').click();
-    //     cy.get('[data-testid="main"]').should('be.visible');
-    // })
+        cy.get('[data-testid="aboutExit"]').click();
+        cy.get('[data-testid="main"]').should('be.visible');
+    })
 
-    // it('Home --> Menu Btn --> Settings Btn', () => {
-    //     cy.get('[data-testid="menuBtn"]').click();
-    //     cy.contains("Settings").click();
+    it('Home --> Menu Btn --> Settings Btn', () => {
+        cy.get('[data-testid="menuBtn"]').click();
+        cy.contains("Settings").click();
 
-    //     cy.get('[data-testid="main"]').should('be.visible');
-    //     cy.get('[data-testid="settingsContainer"]').should('be.visible');
+        cy.get('[data-testid="main"]').should('be.visible');
+        cy.get('[data-testid="settingsContainer"]').should('be.visible');
         
-    //     cy.get('[data-testid="settingsExit"]').click();
-    //     cy.get('[data-testid="main"]').should('be.visible');
-    // })
+        cy.get('[data-testid="settingsExit"]').click();
+        cy.get('[data-testid="main"]').should('be.visible');
+    })
 
     it('Home --> Blog Btn --> About Btn --> Blog --> Settings Btn', () => {
         cy.get('[data-testid="blog-icon"]').click();
@@ -69,9 +71,24 @@ describe('Navigation', () => {
 
         cy.get('[data-testid="menuBtn"]').click();
         cy.contains("Settings").click();
-        cy.get('[data-testid="blogContainer"]').should('have.css', 'display', 'block');
         
         cy.get('[data-testid="settingsExit"]').click();
         cy.get('[data-testid="blogContainer"]').should('be.visible');
+
+        cy.get('[data-testid="home-icon"]').click();
+        cy.get('[data-testid="main"]').should('be.visible');
+        cy.get('[data-testid="blogContainer"]').should('not.be.visible');
+    })
+
+    it('Home --> Blog Btn --> Home Btn --> Settings Btn --> Exit', () => {
+        cy.get('[data-testid="blog-icon"]').click();
+        cy.get('[data-testid="home-icon"]').click();
+
+        cy.get('[data-testid="menuBtn"]').click();
+        cy.contains("Settings").click();
+        cy.get('[data-testid="settingsExit"]').click();
+
+        cy.get('[data-testid="main"]').should('be.visible');
+        cy.get('[data-testid="blogContainer"]').should('not.be.visible');
     })
 })
