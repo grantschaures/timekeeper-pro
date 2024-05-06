4f480671b0db16450c860f19733965c546903482
grantschaures <grantschaures32@gmail.com>
2024-05-06 10:17:02 -0700

    added new post route handler for root

--- app.js
+++ app.js
@@ -71,6 +71,10 @@ app.use(express.static("public")); //app.use() function is used to mount middlew
 // A middleware function (or just middleware) is a function that examines or modifies the request and/or response objects. A middleware function has three parameters: req, res, and next
 // app.use(express.json());
 
+app.post("/", (req, res) => {
+  res.redirect('/'); 
+});
+
 app.get("/login", (req, res) => {
     const filePath = path.join(__dirname, 'public', 'login.html')
     res.sendFile(filePath);
