5efcd295b44b38d59fe755e527e9aab1968cce69
grantschaures <grantschaures32@gmail.com>
2024-06-22 21:19:14 -0700

    more mods

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1540,8 +1540,8 @@ function sendPomodoroDelayNotification(startTimes, counters, timeAmount, chime, 
     //     }
     // }
 
-    alert(isMobile);
-    alert(isIpad);
+    // alert(isMobile);
+    // alert(isIpad);
     // debuggingPopup("cyan");
     // if (!(isMobile || isIpad)) {
     //     new Notification(notificationString);
