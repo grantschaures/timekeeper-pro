53b2d943a6fddae6a48e323fa70670bbff9fda33
grantschaures <grantschaures32@gmail.com>
2024-08-19 18:59:01 -0700

    minor edit

--- editHTML.js
+++ editHTML.js
@@ -249,8 +249,8 @@ const labelDistributionReplacements = [
 
 const metricChartsReplacements = [
     {
-        pattern: /import { setBounds, alterBounds, checkViewportWidth } from '.\/label-distribution.js';/g,
-        replacement: "import { setBounds, alterBounds, checkViewportWidth } from '../minified/label-distribution.min.js';"
+        pattern: /import { setBounds, alterBounds, checkViewportWidth, displayTimeFrame } from '.\/label-distribution.js';/g,
+        replacement: "import { setBounds, alterBounds, checkViewportWidth, displayTimeFrame } from '../minified/label-distribution.min.js';"
     }
 ]
 
